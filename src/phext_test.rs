6074232eb3394cd8de6753ae0497b005db9212d5
Will Bickford <wbic16@gmail.com>
2024-01-26 08:28:02 -0600

    Working on improving test structure

--- src/phext_test.rs
+++ src/phext_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::phext;
+    use std::collections::HashMap;
 
     #[test]
     fn test_coordinate_parsing() {
@@ -10,17 +11,63 @@ mod tests {
         assert_eq!(address, example_coordinate, "Coordinate parsing failed");
     }
 
+    fn test_helper(delim_in: u8, data: HashMap<&str, &str>) -> bool {
+        let mut index = 0;
+        let mut expect1: &str = "not set";
+        let mut expect2: &str = "not set";
+        let mut expect3: &str = "not set";
+        let mut address1: &str = "not set";
+        let mut address2: &str = "not set";
+        let mut address3: &str = "not set";
+        for x in data.keys() {
+            if index == 0 { expect1 = x; address1 = data[x]; index += 1; }
+            if index == 1 { expect2 = x; address2 = data[x]; index += 1; }
+            if index == 2 { expect3 = x; address3 = data[x]; index += 1; }
+        }
+        if index < 3 { return false; }
+
+        let delim = std::str::from_utf8(delim_in).expect("valid utf8");
+        let sample = format!("{expect1}{delim}{expect2}{delim}{expect3}");
+
+        let coord1 = phext::to_coordinate(address1);
+        let coord2 = phext::to_coordinate(address2);
+        let coord3 = phext::to_coordinate(address3);
+
+        let text1 = phext::fetch(&sample, coord1);
+        assert_eq!(text1, expect1, "Fetching text for coord1 failed");
+
+        let text2 = phext::fetch(&sample, coord2);
+        assert_eq!(text2, expect2, "Fetching text for coord2 failed");
+
+        let text3 = phext::fetch(&sample, coord3);
+        assert_eq!(text3, expect3, "Fetching text for coord3 failed");
+
+        return true;
+    }
+
     #[test]
     fn test_scrolls() {
-        let expect1 = "Hello World";
-        let expect2 = "Scroll #2 -- this text will be selected";
-        let expect3 = "Scroll #3 - this text will be ignored";
+        let mut data: HashMap<&str, &str> = std::collections::HashMap::new();
+        data.insert("Hello World", "1.1.1/1.1.1/1.1.1");
+        data.insert("Scroll #2 -- this text will be selected", "1.1.1/1.1.1/1.1.2");
+        data.insert("Scroll #3 - this text will be ignored", "1.1.1/1.1.1/1.1.3");
 
-        let sample = format!("{expect1}\x17{expect2}\x17{expect3}");
+        let result = test_helper(phext::SCROLL_BREAK, data);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_sections() {
+        let expect1 = "Section #1";
+        let expect2 = "Section Part Deux";
+        let expect3 = "Section The Third";
+        let delim = phext::SECTION_BREAK;
+
+        let sample = format!("{expect1}{delim}{expect2}{delim}{expect3}");
 
         let coord1 = phext::to_coordinate("1.1.1/1.1.1/1.1.1");
-        let coord2 = phext::to_coordinate("1.1.1/1.1.1/1.1.2");
-        let coord3 = phext::to_coordinate("1.1.1/1.1.1/1.1.3");
+        let coord2 = phext::to_coordinate("1.1.1/1.1.1/1.2.1");
+        let coord3 = phext::to_coordinate("1.1.1/1.1.1/1.3.1");
 
         let text1 = phext::fetch(&sample, coord1);
         assert_eq!(text1, expect1, "Fetching text for coord1 failed");
