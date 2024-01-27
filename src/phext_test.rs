34a1b82ca5ebc0d8001585b4d170aee71aa34536
Will Bickford <wbic16@gmail.com>
2024-01-26 23:55:34 -0600

    Added shelf tests

--- src/phext_test.rs
+++ src/phext_test.rs
@@ -122,4 +122,15 @@ mod tests {
         let result = test_helper(phext::SERIES_BREAK, data);
         assert_eq!(result, true);
     }
+
+    #[test]
+    fn test_shelf() {
+        let mut data: HashMap<&str, &str> = std::collections::HashMap::new();
+        data.insert("Shelf 1-1-1", "1.1.1/1.1.1/1.1.1");
+        data.insert("Shelf 1-2-1", "1.2.1/1.1.1/1.1.1");
+        data.insert("Shelf 1-3-1", "1.3.1/1.1.1/1.1.1");
+
+        let result = test_helper(phext::SHELF_BREAK, data);
+        assert_eq!(result, true);
+    }
 }
