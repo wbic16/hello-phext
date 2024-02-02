37dce99e94e45a524e2bbba273fb9159d00f1aa6
Will Bickford <wbic16@gmail.com>
2024-02-01 18:07:24 -0600

    Working on adding pack and unpack commands for consuming archives

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
 mod phext;
 mod phext_test;
+use std::env;
 
 fn main() {
     println!("hello-phext v0.0.5");
 
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3
+    {
+        println!("\navailable commands:");
+        println!(" - pack <archive>: packs the given archive as a phext");
+        println!(" - unpack <phext>: unpacks the given phext into your local directory");        
+        return;
+    }
+
+    let command = &args[1];
+    let file = &args[2];
+
+    if command == "pack"
+    {
+        run_pack();
+    }
+
+    if command == "unpack"
+    {
+        run_unpack();
+    }
+    
+    run_example();
+}
+
+fn run_pack() {
+
+}
+
+fn run_unpack() {
+    
+}
+
+fn run_example() {
     let buffer = "Quick Example\x17Second Scroll\x18Second Section\x19Section Chapter";
     let scroll1 = phext::locate(buffer, "1.1.1/1.1.1/1.1.1");
     let scroll2 = phext::locate(buffer, "1.1.1/1.1.1/1.1.2");
