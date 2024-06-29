213374f8e52eb8e90a451018eac5c6356716f2cf
Will Bickford <wbic16@gmail.com>
2024-06-28 23:56:33 -0500

    Noodling with rocket event processing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[macro_use] extern crate rocket;
-use rocket::Request;
-//use rocket::form::Form;
-use rocket::http::{ContentType, Status};
 mod phext;
 mod phext_test;
 use std::{fs::{self, File}, io::Write};
+use rocket::Request;
+use rocket::http::Status;
+use rocket::http::ContentType;
 
 #[get("/api/<world>/<coordinate>")]
 fn index(world: &str, coordinate: &str) -> (ContentType, String) {
@@ -16,17 +16,38 @@ fn index(world: &str, coordinate: &str) -> (ContentType, String) {
 
   return (ContentType::HTML, response);
 }
-
-#[derive(Debug, PartialEq, Eq, FromForm)]
-struct PhextScroll<'r> {
-    scroll: &'r str
+/*
+#[derive(Default, Debug, PartialEq, Eq)]
+struct Subspace {
+    buffer: String
 }
+use rocket::{Request, Data};
+use rocket::outcome::Outcome;
+use rocket::data::FromData;
+use rocket::http::{Status, ContentType};
 
+const LIMIT: u64 = 1024*1024*100; // 100 MB or less
+
+impl FromData for Subspace {
+  type Error = String;
+
+  fn from_data(req: &Request, data: Data) -> Outcome<Self, String, String> {
+      let mut result = String::new();
+      if let Err(e) = data.open().take(LIMIT).read_to_string(&mut result) {
+        return rocket::Failure((Status::InternalServerError, format!("{:?}", e)));
+      }
+
+      let mut output: Subspace = Default::default();
+      output.buffer = result;
+      rocket::Success(output)
+  }
+}
+*/
 #[post("/api/<world>/<coordinate>")]
 //fn set(world: &str, coordinate: &str, scroll: Form<PhextScroll>) -> (ContentType, String) {
 fn set(world: &str, coordinate: &str) -> (ContentType, String) {
   let filename = world.to_owned() + ".phext";
-  let message = "pending"; // scroll;
+  let message = "TBD"; // scroll;
   let file = File::create(&filename);
   let required = "Unable to locate ".to_owned() + &filename;
   let _result = file.expect(&required).write_all(message.as_bytes());
