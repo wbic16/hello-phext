8aed70ef453b45768fc2658aeb9f5f146e9da734
Will Bickford <wbic16@gmail.com>
2024-07-01 18:29:01 -0500

    Fixed compile errors; starting to test the data guard

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::{fs::{self, File}, io::Write};
 use rocket::Request;
 use rocket::http::Status;
 use rocket::http::ContentType;
-//use rocket::tokio::io::AsyncReadExt;
-//use rocket::outcome::Outcome::Success;
-//use rocket::form::Form;
+use rocket::form::Form;
 
 #[get("/api/<world>/<coordinate>")]
 fn index(world: &str, coordinate: &str) -> (ContentType, String) {
@@ -20,39 +18,20 @@ fn index(world: &str, coordinate: &str) -> (ContentType, String) {
   return (ContentType::HTML, response);
 }
 
-#[derive(Default, Debug, PartialEq, Eq, FromForm)]
-struct Subspace<'r> {
+#[derive(Default, Debug, PartialEq, Eq, FromForm, Responder)]
+struct Subspace {
     buffer: String,
-    r#type: &'r str
 }
-/*
-use rocket::Data;
-use rocket::outcome::Outcome;
-use rocket::data::FromData;
-
-// see: https://rocket.rs/guide/v0.5/responses/
-// see: https://rocket.rs/guide/v0.5/requests/#request-guards
-// see: https://api.rocket.rs/v0.5/rocket/data/trait.FromData
-#[rocket::async_trait]
-impl<'r> FromData<'r> for Subspace<'r> {
-  type Error = String;
-
-  async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<Status, String, String> {
-      let mut result = String::new();
-      let mut stream = data.open(1.mebibytes());
-      stream.read_to_string(&mut result);
-
-      let mut output: Subspace = Default::default();
-      output.buffer = result;
-      Success(Status::Ok, output);
+impl Subspace {
+  pub fn as_bytes(&self) -> &[u8] {
+    return self.buffer.as_bytes();
   }
 }
-*/
-#[post("/api/<world>/<coordinate>")]
-//fn set(world: &str, coordinate: &str, scroll: Form<Subspace>) -> (ContentType, String) {
-fn set(world: &str, coordinate: &str) -> (ContentType, String) {
+
+#[post("/api/<world>/<coordinate>", data="<scroll>")]
+fn set(world: &str, coordinate: &str, scroll: Form<Subspace>) -> (ContentType, String) {
   let filename = world.to_owned() + ".phext";
-  let message = "TBD"; // scroll;
+  let message = scroll;
   let file = File::create(&filename);
   let required = "Unable to locate ".to_owned() + &filename;
   let _result = file.expect(&required).write_all(message.as_bytes());
