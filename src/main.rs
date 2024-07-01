28b9c79e5948df1aab748b97865cddb1eb6b840a
Will Bickford <wbic16@gmail.com>
2024-06-30 23:40:29 -0500

    Learning rust + rocket at the same time might be a mistake...

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 mod phext;
 mod phext_test;
 use std::{fs::{self, File}, io::Write};
-use rocket::{data::ToByteUnit, Request};
+use rocket::Request;
 use rocket::http::Status;
 use rocket::http::ContentType;
-use rocket::tokio::io::AsyncReadExt;
-use rocket::outcome::Outcome::Success;
+//use rocket::tokio::io::AsyncReadExt;
+//use rocket::outcome::Outcome::Success;
+//use rocket::form::Form;
 
 #[get("/api/<world>/<coordinate>")]
 fn index(world: &str, coordinate: &str) -> (ContentType, String) {
@@ -19,30 +20,36 @@ fn index(world: &str, coordinate: &str) -> (ContentType, String) {
   return (ContentType::HTML, response);
 }
 
-#[derive(Default, Debug, PartialEq, Eq)]
-struct Subspace {
-    buffer: String
+#[derive(Default, Debug, PartialEq, Eq, FromForm)]
+struct Subspace<'r> {
+    buffer: String,
+    r#type: &'r str
 }
+/*
 use rocket::Data;
 use rocket::outcome::Outcome;
 use rocket::data::FromData;
-/*
-impl FromData<'_> for Subspace {
+
+// see: https://rocket.rs/guide/v0.5/responses/
+// see: https://rocket.rs/guide/v0.5/requests/#request-guards
+// see: https://api.rocket.rs/v0.5/rocket/data/trait.FromData
+#[rocket::async_trait]
+impl<'r> FromData<'r> for Subspace<'r> {
   type Error = String;
 
-  fn from_data(req: &Request, data: Data) -> Outcome<Self, String, String> {
+  async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<Status, String, String> {
       let mut result = String::new();
       let mut stream = data.open(1.mebibytes());
       stream.read_to_string(&mut result);
 
       let mut output: Subspace = Default::default();
       output.buffer = result;
-      Success(output)
+      Success(Status::Ok, output);
   }
 }
 */
 #[post("/api/<world>/<coordinate>")]
-//fn set(world: &str, coordinate: &str, scroll: Form<PhextScroll>) -> (ContentType, String) {
+//fn set(world: &str, coordinate: &str, scroll: Form<Subspace>) -> (ContentType, String) {
 fn set(world: &str, coordinate: &str) -> (ContentType, String) {
   let filename = world.to_owned() + ".phext";
   let message = "TBD"; // scroll;
